4fda7b9e1f524fab879e424272787a43774ab509
mahmuda-mahmi <mahmudapakter@gmail.com>
2024-06-28 03:07:38 +0600

    sections added

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,3 +8,41 @@ $(function () {
         }
     });
 })
+
+$('.responsive').slick({
+    dots: false,
+    infinite: true,
+    prevArrow:'<button type="button" class="slick-prev text-2xl hover:bg-[#12372A] border border-[#12372A] rounded-full h-10 w-10 leading-10 bg-white hover:text-white"><i class="fa-sharp fa-light fa-caret-left"></i></button>',
+    nextArrow:'<button type="button" class="slick-next text-2xl hover:bg-[#12372A] border border-[#12372A] rounded-full h-10 w-10 leading-10 bg-white hover:text-white"><i class="fa-sharp fa-light fa-caret-right"></i></button>',
+    speed: 2000,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+    ]
+  });
